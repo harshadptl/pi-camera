b739ccce6f3c82d17f3b3396cdc464e9f7c817ff
Nick Markwell <nick@duckinator.net>
2013-01-02 04:22:57 -0500

    Rewrote so it only uses one process. 8FPS faster.

--- camera.rb
+++ camera.rb
@@ -2,21 +2,45 @@
 
 require 'hornetseye_v4l2'
 require 'hornetseye_rmagick'
-   
-cam = Hornetseye::V4L2Input.new '/dev/video0' do |modes|
-  areas = modes.map { |mode| [mode[1] * mode[2], mode] }
-  areas.sort.last[1]
+
+class Camera
+  def initialize
+    @cam = Hornetseye::V4L2Input.new '/dev/video0' do |modes|
+      areas = modes.map { |mode| [mode[1] * mode[2], mode] }
+      areas.sort.last[1]
+    end
+    10.times { @cam.read }
+
+    @capture = -1
+    poll
+  end
+
+  def capture
+    @capture += 1
+
+    print "Capture #{@capture}... "
+
+    frame = @cam.read
+    img = frame.to_type Hornetseye::UBYTERGB
+    img.save_ubytergb "capture_#{@capture}.jpeg"
+    puts 'Done.'
+  end
+
+  def poll
+    # Poll the camera, to keep it adjusting to the lighting.
+    Thread.new do
+      loop do
+        sleep 2
+        @cam.read
+      end
+    end
+  end
 end
-10.times { cam.read }
- 
-capture = -1
 
-Signal.trap("USR1") do
-  capture += 1
+camera = Camera.new
 
-  frame = cam.read
-  img = frame.to_type Hornetseye::UBYTERGB
-  img.save_ubytergb "capture_#{capture}.jpeg"
+Thread.new { sleep 1; exit }
+
+loop do
+  camera.capture
 end
-
-sleep
